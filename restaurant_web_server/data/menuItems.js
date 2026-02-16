dc51a10445bdb31466294349841758ce6825e2d7
Sarztak <sar3142168@gmail.com>
2026-02-15 22:04:32 -0600

    a dead simple web server and a few pages

---
+++ restaurant_web_server/data/menuItems.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        name: "Broccoli Pie",
+        description: "A green pie with an earthy crust",
+        cost: 12.99,
+    },
+
+    {
+        name: "Eggplant Smoothie",
+        description: "A purple shake with an earthy crust",
+        cost: 5.99,
+    },
+
+    {
+        name: "Watermelon Shushi",
+        description: "A red roll with atmospheric sweetness",
+        cost: 8.99,
+    },
+]
---
+++ restaurant_web_server/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import workingHours from "./data/workingHours.js";
+import menuItems from "./data/menuItems.js";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "pug");
+app.set("views", "./views")
+
+app.get('/', (req, res) => {
+    res.render("index", {name: "What's Fare is Fair"});
+});
+
+app.get('/menu', (req, res) => {
+    res.render("menu", {menuItems});
+});
+
+app.get('/hours', (req, res) => {
+    const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+    res.render("hours", {workingHours, days});
+});
+
+app.listen(port, () => {
+    console.log(`Web Server is listening at localhost:${port}`);
+});
